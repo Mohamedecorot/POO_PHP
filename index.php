e911f07c2b02945e201b8165dbc31377da3dc3df
guerroui.m@gmail.com <guerroui.m@gmail.com>
2020-12-17 09:36:47 +0100

    première class

--- index.php
+++ index.php
@@ -1,2 +1,30 @@
 <?php
-require 'personnage.php';
+require 'Personnage.php';
+
+$merlin = new Personnage("Merlin");
+
+// Si on veut faire crier le personnage :
+$merlin->crier();
+
+//Si on veut attribuer une nouvelle valeur à une propriété
+$merlin->vie = 100;
+//ou
+$merlin->regenerer(5);
+
+//si on a un nouvel objet (personnage), il est indépendant des autres objets
+
+// sans constructeur
+//$harry = new Personnage();
+//$harry->nom = "Harry";
+
+// Avec constructeur
+$harry = new Personnage("Harry");
+$harry->regenerer();
+
+$merlin->attaque($harry);
+
+if($harry->mort()) {
+    echo 'Harry est mort';
+} else {
+    echo 'Harry a survécu avec ' . $harrt->vie . ' de vie';
+}
