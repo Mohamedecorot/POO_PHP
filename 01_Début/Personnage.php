a038a46ac5f978f132dcf25c30b67e5582a2db83
guerroui.m@gmail.com <guerroui.m@gmail.com>
2020-12-17 14:15:19 +0100

    Exemple concret: Form

---
+++ 01_Début/Personnage.php
@@ -0,0 +1,69 @@
+<?php
+
+class Personnage{
+
+    private $vie = 80;
+    private $atk = 20;
+    private $name;
+    private $age = 30;
+
+    public function __construct($nom)
+    {
+        $this->nom = $nom;
+    }
+
+    // les getters permettent de retourner des propriétés privées
+    public function getAge()
+    {
+        return $this->age;
+    }
+
+    public function getNom()
+    {
+        return $this->nom;
+    }
+
+    // les setters c'est comme les getters mais en sens inverse
+    public function setNom($nom)
+    {
+        $this->nom = $nom;
+    }
+
+    public function getVie()
+    {
+        return $this->vie;
+    }
+
+    public function getAtk()
+    {
+        return $this->atk;
+    }
+    public function crier()
+    {
+        echo "A l'attaque!";
+    }
+
+    public function regenerer($vie = null)
+    {
+        if(is_null($vie)) {
+            //$this faire référence à l'instance en cours
+            $this->vie = 100;
+        } else {
+            $this->vie = $this->vie + $vie;
+            // équivalent à $this->vie += $vie;
+        }
+    }
+
+    public function mort(): bool
+    {
+        return $this->vie <= 0;
+    }
+
+    public function attaque($cible)
+    {
+        /*$this attaquant
+        $cible défenseur
+        defenseur.vie -= attaquant.atk*/
+        $cible->vie -= $this->atk;
+    }
+}
--- index.php
+++ index.php
@@ -1,34 +1,14 @@
 <?php
-require 'Personnage.php';
 
-$merlin = new Personnage("Merlin");
+require 'form.php';
 
-// Si on veut faire crier le personnage :
-$merlin->crier();
+$form = new Form($_POST);
+?>
 
-//Si on veut attribuer une nouvelle valeur à une propriété
-//$merlin->vie = 100;
-//ou
-$merlin->regenerer(5);
-
-echo $merlin->getVie();
-
-//si on a un nouvel objet (personnage), il est indépendant des autres objets
-
-// sans constructeur
-//$harry = new Personnage();
-//$harry->nom = "Harry";
-
-// Avec constructeur
-$harry = new Personnage("Harry");
-$harry->regenerer();
-
-$merlin->attaque($harry);
-
-if($harry->mort()) {
-    echo 'Harry est mort';
-} else {
-    echo 'Harry a survécu avec ' . $harrt->vie . ' de vie';
-}
-
-//test de configuration
+<form action="#" method="post">
+    <?php
+        echo $form->input('username');
+        echo $form->input('password');
+        echo $form->submit();
+        ?>
+</form>
