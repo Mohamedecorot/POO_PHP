b3b6a801902e5f05689ef84ccd2c8dcd6ae9e15a
guerroui.m@gmail.com <guerroui.m@gmail.com>
2020-12-21 16:35:51 +0100

    TP: reprise du blog: récupération des catégories des articles

--- nouveau_blog/pages/home.php
+++ nouveau_blog/pages/home.php
@@ -24,6 +24,7 @@
 <?php foreach (App\Table\Article::getLast() as $post): ?>
 
 <h2><a href="<=? $post->url ?>"><?= $post->titre; ?></a></h2>
+<p><em><?= $post->categorie; ?></em></p>
 <p><?= $post->getExtrait(); ?></p>
 
 <?php endforeach; ?>
