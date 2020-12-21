0b3dbb87c081022f2eb6a521ad5564864b9f76d2
guerroui.m@gmail.com <guerroui.m@gmail.com>
2020-12-21 16:25:42 +0100

    TP: reprise du blog: ajout de la class getDb

--- nouveau_blog/pages/home.php
+++ nouveau_blog/pages/home.php
@@ -21,7 +21,7 @@
 
 ?>
 
-<?php foreach ($db->query('SELECT * FROM articles', 'App\Table\Article') as $post): ?>
+<?php foreach (App\App::getDb()->query('SELECT * FROM articles', 'App\Table\Article') as $post): ?>
 
 <h2><a href="<=? $post->url ?>"><?= $post->titre; ?></a></h2>
 <p><?= $post->getExtrait(); ?></p>
