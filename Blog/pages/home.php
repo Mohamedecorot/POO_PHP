e7e5167cebd27373eac1762715a04a54a09b73a8
guerroui.m@gmail.com <guerroui.m@gmail.com>
2020-12-22 14:36:33 +0100

    TP: reprise du blog à zero: récupération de la bdd avec pdo

--- Blog/pages/home.php
+++ Blog/pages/home.php
@@ -1,3 +1,19 @@
 <H1>home</H1>
 
 <p><a href="index.php?p=single">single</a></p>
+
+<?php
+//pour se connecter à la bdd
+$pdo = new PDO ('mysql:dbname=blog;host=localhost', 'root', 'root');
+
+//pour voir les erreurs
+$pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+
+//requete sql
+//$count = $pdo->exec('INSERT INTO articles SET titre="mon titre", contenu="mon contenu", date="' . date('Y-m-d H:i:s') .'"');
+
+//pour récuperer les articles
+$resultat = $pdo->query('SELECT * FROM articles');
+$datas = $resultat->fetchAll(PDO::FETCH_OBJ);
+var_dump($datas);
+?>
