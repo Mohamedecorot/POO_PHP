c245d11264e705071f250e6f1c8f17220d66e1fa
guerroui.m@gmail.com <guerroui.m@gmail.com>
2020-12-22 16:31:34 +0100

    TP: reprise du blog à zero: recuperation de la liste des catégories

--- Blog/App/Table/Table.php
+++ Blog/App/Table/Table.php
@@ -7,12 +7,21 @@ class Table {
 
     protected static $table;
 
+    private static function getTable()
+    {
+        if(static::$table === null) {
+            $class_name = explode('\\', get_called_class());
+            static::$table = strtolower(end($class_name)) . 's';
+        }
+        //die (static::$table);
+        return static::$table;
+    }
 
     public static function all()
     {
         return App::getDb()->query("
         SELECT *
-        FROM " . static::$table ."
+        FROM " . static::getTable() ."
         ", get_called_class());
     }
 
