bf2e5e564fe4b78020967b1c50c76f40401c2d7f
guerroui.m@gmail.com <guerroui.m@gmail.com>
2020-12-25 16:30:39 +0100

    TP: Back-end/Administration: vérification des identifiants deconnexion

--- Blog/Core/Auth/DBAuth.php
+++ Blog/Core/Auth/DBAuth.php
@@ -20,7 +20,14 @@ class DbAuth {
     public function login($username, $password)
     {
         $user = $this->db->prepare('SELECT * FROM users WHERE username = ?', [$username], null, true);
-        var_dump($user);
+        if($user){
+            if($user->password === sha1($password)){
+                $_SESSION['auth'] = $user->id;
+                return true;
+            }
+        }
+        return false;
+        //var_dump($user);
     }
 
     public function logged()
--- Blog/pages/users/login.php
+++ Blog/pages/users/login.php
@@ -2,6 +2,18 @@
 
 use Core\Html\BootstrapForm;
 
+if(!empty($_POST)){
+    $auth = new \Core\Auth\DBAuth(App::getInstance()->getDb());
+    if($auth->login($_POST['username'], $_POST['password'])){
+        header('Location: admin.php');
+    } else {
+        ?>
+        <div class="alert alert-danger">
+            Identifiants incorrect
+        </div>
+        <?php
+    }
+}
 $form = new BootstrapForm($_POST);
 
 ?>
