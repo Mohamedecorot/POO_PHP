bcf9d6c5483bd1aab83874e7293f607f7c8850a4
guerroui.m@gmail.com <guerroui.m@gmail.com>
2020-12-25 16:45:25 +0100

    TP: Back-end/Administration: affichage des articles sur la page admin

--- Blog/pages/admin/posts/index.php
+++ Blog/pages/admin/posts/index.php
@@ -1,1 +1,26 @@
-<h1>Administation</h1>
+<?php
+$posts = App::getInstance()->getTable('Post')->all();
+?>
+
+<h1>Administation des articles</h1>
+
+<table class="table">
+    <thead>
+        <tr>
+            <td>ID</td>
+            <td>Titre</td>
+            <td>Actions</td>
+        </tr>
+    </thead>
+    <tbody>
+        <?php foreach($posts as $post): ?>
+            <tr>
+                <td><?= $post->id; ?></td>
+                <td><?= $post->titre; ?></td>
+                <td>
+                    <a class="btn btn-primary" href="p=?posts.edit&id=<?= $post->id; ?>">Editer</a>
+                </td>
+            </tr>
+        <?php endforeach ?>
+    </tbody>
+</table>
