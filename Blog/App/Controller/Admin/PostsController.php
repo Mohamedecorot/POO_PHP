acd1821974eb3cadd0f54abd2a2919588ca03d59
guerroui.m@gmail.com <guerroui.m@gmail.com>
2020-12-29 13:50:21 +0100

    TP: MVC : ajout de la methode add dans le postcontroller

--- Blog/App/Controller/Admin/PostsController.php
+++ Blog/App/Controller/Admin/PostsController.php
@@ -1,6 +1,9 @@
 <?php
 namespace App\Controller\Admin;
 
+use App;
+use Core\Html\BootstrapForm;
+
 
 class PostsController extends \App\Controller\AppController{
 
@@ -16,4 +19,30 @@ class PostsController extends \App\Controller\AppController{
         $this->render('admin.posts.index', compact('posts'));
     }
 
+    public function add()
+    {
+        if(!empty($_POST)){
+            $result = $this->Post->create([
+                'titre' => $_POST['titre'],
+                'contenu' => $_POST['contenu'],
+                'category_id' => $_POST['category_id']
+            ]);
+            if($result) {
+                header('Location: admin.php?p=admin.posts.edit&id=' . App::getInstance()->getDb()->lastInsertId());
+            }
+        }
+        $this->loadModel('Category');
+        $categories = $this->Category->extract('id', 'titre');
+        $form = new BootstrapForm($_POST);
+    }
+
+    public function edit()
+    {
+
+    }
+
+    public function delete()
+    {
+
+    }
 }
