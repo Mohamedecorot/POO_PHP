39edb8ab32b5a40c028fa83707ab92e2f29043dd
guerroui.m@gmail.com <guerroui.m@gmail.com>
2020-12-29 14:08:10 +0100

    TP: MVC : restructuration des liens du blog

--- Blog/public/index.php
+++ Blog/public/index.php
@@ -39,26 +39,39 @@ App::load();
 if(isset($_GET['p'])){
     $page = $_GET['p'];
 } else {
-    $page = 'home';
+    $page = 'posts.index';
 }
 
-//ob_start();
-if($page === 'home') {
-    $controller = new \App\Controller\PostsController();
-    $controller->index();
-} elseif ($page === 'posts.category') {
-    $controller = new \App\Controller\PostsController();
-    $controller->category();
-} elseif ($page === 'posts.show') {
-    $controller = new \App\Controller\PostsController();
-    $controller->show();
-} elseif ($page === 'login') {
-    $controller = new \App\Controller\UsersController();
-    $controller->login();
-} elseif ($page === 'admin.posts.index') {
-    $controller = new \App\Controller\Admin\PostsController();
-    $controller->index();
+$page = explode('.', $page);
+if($page[0] == 'admin'){
+    $controller = '\App\Controller\admin\\' . ucfirst($page[1]) . 'Controller';
+    $action = $page[2];
+} else {
+    $controller = '\App\Controller\\' . ucfirst($page[0]) . 'Controller';
+    $action = $page[1];
+
 }
+$controller = new $controller();
+$controller->$action();
+
+
+//ob_start();
+// if($page === 'home') {
+//     $controller = new \App\Controller\PostsController();
+//     $controller->index();
+// } elseif ($page === 'posts.category') {
+//     $controller = new \App\Controller\PostsController();
+//     $controller->category();
+// } elseif ($page === 'posts.show') {
+//     $controller = new \App\Controller\PostsController();
+//     $controller->show();
+// } elseif ($page === 'login') {
+//     $controller = new \App\Controller\UsersController();
+//     $controller->login();
+// } elseif ($page === 'admin.posts.index') {
+//     $controller = new \App\Controller\Admin\PostsController();
+//     $controller->index();
+// }
 
 // $content = ob_get_clean();
 // require ROOT . '/pages/templates/default.php';
