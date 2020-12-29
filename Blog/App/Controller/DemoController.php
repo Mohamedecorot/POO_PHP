a58a53b6677166e04fbb8389603623c0aa017524
guerroui.m@gmail.com <guerroui.m@gmail.com>
2020-12-29 16:33:58 +0100

    TP: Fluent: tuto

---
+++ Blog/App/Controller/DemoController.php
@@ -0,0 +1,22 @@
+<?php
+namespace App\Controller;
+
+use Core\Database\QueryBuilder;
+
+class DemoController extends AppController{
+
+    public function index()
+    {
+        $query = new QueryBuilder();
+        // $query->where('id = 1');
+        // $query->select('id', 'titre', 'contenu');
+        // $query->from('article', 'Post');
+        // echo $query->getQuery();
+        //Equivalent en fluent à ça :
+        echo $query->select('id', 'titre', 'contenu')
+            ->from('article', 'Post')
+            ->where('Post.category_id = 1')
+            ->where('Post.date > NOW()');
+    }
+
+}
