8adbe6778c8b2aee81a8f2989ad2e269bc8ab96a
guerroui.m@gmail.com <guerroui.m@gmail.com>
2021-01-04 16:47:38 +0100

    Event: tests sur la methode on

--- Event/spec/Emitter.spec.php
+++ Event/spec/Emitter.spec.php
@@ -1,6 +1,7 @@
 <?php
 
 use Event\Emitter;
+use Kahlan\Plugin\Double;
 
 describe(Emitter::class, function () {
 
@@ -10,5 +11,21 @@ describe(Emitter::class, function () {
         expect($instance)->toBe(Emitter::getInstance());
     });
 
+});
+
+describe('::on', function () {
+
+    it('should trigger the listened event', function () {
+        $emitter = Emitter::getInstance();
+        $listener = Double::instance();
+        $comment = ['name' => 'John'];
+
+        expect($listener)->toReceive('onNewComment')->times(3)->with($comment);
+
+        $emitter->on('Comment.created', [$listener, 'onNewComment']);
+        $emitter->emit('Comment.created', $comment);
+        $emitter->emit('Comment.created', $comment);
+        $emitter->emit('Comment.created', $comment);
+    });
 
 });
